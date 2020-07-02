f38eb97be717e6d6f20c30c744909ec29b087a96
Adoilson Freitas <adoilsonfreitas@gmail.com>
2020-07-01 22:36:08 -0300

    Updates

---
+++ precache-manifest.dfec7484de7b40daa2cb5096f088d41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b953d4fac0c4ef627da71c4a0729a892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2dd9a993b420c50ce02",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "f2dd9a993b420c50ce02",
+    "url": "/static/js/2.3b90c826.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b90c826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6e73fa3c24fa025f3e",
+    "url": "/static/js/main.24511367.chunk.js"
+  },
+  {
+    "revision": "1e967e60c8804a474105",
+    "url": "/static/js/runtime-main.9050acbe.js"
+  }
+]);
